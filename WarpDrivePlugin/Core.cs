1e55a2a12d1adf5727c54e8cc6e4dc42ceed7e9e
Joseph Pearson <chessmaster42@gmail.com>
2014-07-05 16:09:06 -0500

    -Initial commit

---
+++ WarpDrivePlugin/Core.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SEModAPIExtensions.API.Plugin.Events;
+
+using SEModAPIInternal.API.Entity.Sector.SectorObject;
+
+namespace SEModAPIExtensions.API.Plugin
+{
+	public class Core : PluginBase
+	{
+		#region "Constructors and Initializers"
+
+		public Core()
+		{
+			Console.WriteLine("WarpDrivePlugin '" + Id.ToString() + "' constructed!");
+		}
+
+		public override void Init()
+		{
+			Console.WriteLine("WarpDrivePlugin '" + Id.ToString() + "' initialized!");
+		}
+
+		#endregion
+
+		#region "Methods"
+
+		public override void Update()
+		{
+			//Console.WriteLine("WarpDrivePlugin '" + Id.ToString() + "' updated!");
+		}
+
+		#endregion
+	}
+}
